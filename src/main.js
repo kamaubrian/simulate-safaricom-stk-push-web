4582282d53c9e6ea21fd42e15e576912203467e8
mtotodev05 <mtotodev05@gmail.com>
2019-03-16 15:02:36 +0300

    Fix router issues

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import './plugins/vuetify'
 import App from './App.vue'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
