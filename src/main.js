66daf1869409c41360fef45df902b9e226a65b5e
mtotodev05 <mtotodev05@gmail.com>
2019-03-16 14:19:49 +0300

    Setup libraries

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
+import '@babel/polyfill'
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
 
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify, {
+  options: {
+    customProperties: true
+  },
+  iconfont: 'mdi',
+})
