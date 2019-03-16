246896a14b76d034c17ee8a10dd32deb58ba8e4b
mtotodev05 <mtotodev05@gmail.com>
2019-03-16 16:05:50 +0300

    include actions and getters

--- src/store/index.js
+++ src/store/index.js
@@ -15,5 +15,31 @@ export const store = new Vuex.Store({
         }],
         loading: null,
         error: ''
+    },
+    mutations: {
+        setLoading(state, payload) {
+            state.loading = payload
+        },
+        unSetLoading(state) {
+            state.loading = false;
+        },
+        setError(state, payload) {
+            state.error = payload;
+        },
+        unSetError(state) {
+            state.error = false;
+        },
+        setPayment(state, payload) {
+            state.payments = payload
+        },
+        unSetPayment(state) {
+            state.payments = [];
+        }
+    },
+    actions: {
+
+    },
+    getters: {
+
     }
 });
