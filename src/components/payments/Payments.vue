c04b9c745f8407aadd15f60aad0c04c86644a088
mtotodev05 <mtotodev05@gmail.com>
2019-03-16 17:39:14 +0300

    Fetch transactions

--- src/components/payments/Payments.vue
+++ src/components/payments/Payments.vue
@@ -1,16 +1,88 @@
-<template>
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
     <v-layout>
         <v-container grid-list-md fluid>
-            <v-layout row wrap v-if="">
-
+            <v-layout row wrap v-if="loading">
+                <v-flex xs12 class="text-xs-center">
+                    <v-progress-circular indeterminate class="primary--text"
+                                         :width="7"
+                                         :size="70"
+                    ></v-progress-circular>
+                </v-flex>
             </v-layout>
-            <v-layout row wrap v-else>
+            <v-layout row wrap v-else class="text-xs-center">
+                <v-flex xs12>
+                    <v-card class="elevation-4">
+                        <v-card-title>
+                            <h4 class="primary--text">Transactions</h4>
+                            <v-spacer></v-spacer>
+                            <v-text-field
+                                    name="Search"
+                                    label="Search Payment"
+                                    v-model="search"
+                                    ></v-text-field>
+                        </v-card-title>
+                        <v-card-text>
+                            <v-data-table
+                                    :headers="headers"
+                                    :items="payments"
+                                    :expand="expand"
+                                    :search="search"
+                            >
+                                <template slot="items" slot-scope="props">
+                                    <td class="text-xs-center">{{ props.item.emailAddress }}</td>
+                                    <td class="text-xs-center">{{ props.item.phoneNumber }}</td>
+                                    <td class="text-xs-center">{{ props.item.transactionId }}</td>
+                                    <td class="text-xs-center">{{ props.item.transactionTime }}</td>
+                                </template>
 
+                            </v-data-table>
+                        </v-card-text>
+                    </v-card>
+                </v-flex>
             </v-layout>
         </v-container>
     </v-layout>
 </template>
 
 <script>
-    export default {}
+    export default {
+        data() {
+            return {
+                expand: false,
+                search: '',
+                headers: [
+                    {
+                        text: 'Email Address',
+                        align: 'left',
+                        sortable: false,
+                        value: 'emailAddress'
+                    },
+                    {text: 'Transaction Code', value: 'transactionId'},
+                    {text: 'Phone Number', value: 'phoneNumber'},
+                    {text: 'Transaction Time', value: 'transactionTime'}
+                ],
+                payments: [{
+                    _id: '',
+                    emailAddress: '',
+                    phoneNumber: '',
+                    transactionId: '',
+                    transactionTime: ''
+                }]
+            }
+        },
+        computed: {
+            loading() {
+                return this.$store.getters.loading
+            }
+        },
+        async created() {
+            await this.$store.dispatch('fetchRecords');
+            this.payments = await this.$store.getters.payments
+        },
+        async mounted() {
+            await this.$store.dispatch('fetchRecords');
+            this.payments = await this.$store.getters.payments
+        }
+
+    }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../components/Home'
-
+import Payments from '../components/payments/Payments'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +10,11 @@ export default new Router({
             path: '/home',
             name: 'Home',
             component: Home
+        },
+        {
+            path: '/payments',
+            name: 'Payments',
+            component: Payments
         }
 
     ], mode: 'history'
